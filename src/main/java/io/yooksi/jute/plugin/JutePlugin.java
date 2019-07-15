64c136a408515f086251f8152b9cf5b6b3dd684b
yooksi <yooksi@zoho.com>
2019-07-15 06:16:20 +0200

    Start developing Jute plugin

---
+++ src/main/java/io/yooksi/jute/plugin/JutePlugin.java
@@ -0,0 +1,13 @@
+package io.yooksi.jute.plugin;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class JutePlugin implements Plugin<Project> {
+
+    @Override
+    public void apply(Project target) {
+
+
+    }
+}
