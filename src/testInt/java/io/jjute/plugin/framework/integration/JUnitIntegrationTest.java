9884a58b672308907789e0571ec4a70a19af53f8
yooksi <yooksi@zoho.com>
2019-07-25 16:17:54 +0200

    Add one integration and functional test

---
+++ src/testInt/java/io/jjute/plugin/framework/integration/JUnitIntegrationTest.java
@@ -0,0 +1,50 @@
+package io.jjute.plugin.framework.integration;
+
+import io.jjute.plugin.framework.PluginConfig;
+import io.jjute.plugin.testsuite.FunctionalTest;
+import org.junit.jupiter.api.Test;
+
+@SuppressWarnings("WeakerAccess")
+public class JUnitIntegrationTest extends FunctionalTest {
+
+    private static final String JUNIT_INTEGRATION = PluginConfig.Property.JUNIT_INTEGRATION.getName();
+
+    @Test
+    public void shouldAddProjectDependencies() {
+
+        initAndWriteToBuildFile(new String[] {
+                "task verifyJUnitIntegration {",
+                "   if (!(test.getTestFramework() instanceof org.gradle.api.internal.tasks.testing." +
+                        "junitplatform.JUnitPlatformTestFramework))",
+                "       throw new RuntimeException(\"JUnit was not configured as a project test framework.\")",
+                "}",
+        }); createRunnerForPlugin().withProperty(JUNIT_INTEGRATION, "true").build();
+    }
+
+    @Test
+    public void shouldConfigureProjectTestFramework() {
+
+        initAndWriteToBuildFile(new String[] {
+                "task verifyJUnitIntegration {",
+                "   java.util.Set<Dependency> dependencies = io.jjute.plugin.framework.util." +
+                        "ProjectUtils.getProjectDependencies(project)",
+                "   String[] junitDependencies = [",
+                "       io.jjute.plugin.framework.integration.JUnitIntegration.API,",
+                "       io.jjute.plugin.framework.integration.JUnitIntegration.ENGINE",
+                "   ]",
+                "   boolean[] foundDependency = new boolean[junitDependencies.length]",
+                "   dependencies.each { d -> ",
+                "       String notation = io.jjute.plugin.framework.util.ProjectUtils.getDependencyNotation(d)",
+                "       for (int i = 0; i < foundDependency.length; i++) {",
+                "           if (junitDependencies[i].equals(notation))",
+                "               foundDependency[i] = true",
+                "       }",
+                "   }",
+                "   for (int i = 0; i < foundDependency.length; i++) {",
+                "       if (foundDependency[i] == false)",
+                "           throw new RuntimeException(\"Unable to find JUnit dependency: \" + junitDependencies[i])",
+                "   }",
+                "}"
+        }); createRunnerForPlugin().build();
+    }
+}
---
+++ src/testInt/java/io/jjute/plugin/framework/util/ProjectDependencyTest.java
@@ -0,0 +1,62 @@
+package io.jjute.plugin.framework.util;
+
+import io.jjute.plugin.testsuite.IntegrationTest;
+import org.gradle.api.artifacts.Dependency;
+import org.gradle.api.artifacts.dsl.DependencyHandler;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+@SuppressWarnings("WeakerAccess")
+public class ProjectDependencyTest extends IntegrationTest {
+
+    private static final String apacheLang = "org.apache.commons:commons-lang3:3.9";
+    private static final String apacheMath = "org.apache.commons:commons-math3:3.6.1";
+
+    @BeforeEach
+    public void addSampleApacheDependencies() {
+
+        DependencyHandler handler = project.getDependencies();
+        handler.add("implementation", apacheLang);
+        handler.add("implementation", apacheMath);
+    }
+
+    @Test
+    public void whenGettingTestTaskShouldNotThrowException() {
+        Assertions.assertDoesNotThrow(() -> ProjectUtils.getTestTask(project));
+    }
+
+    @Test
+    public void shouldFindDeclaredProjectDependencies() {
+
+        String g1, n1, v1, g2, n2, v2;
+
+        g1 = "org.apache.commons"; n1 = "commons-lang3"; v1 = "3.9";
+        g2 = "org.apache.commons"; n2 = "commons-math3"; v2 = "3.6.1";
+
+        Assertions.assertTrue(ProjectUtils.projectHasDependency(project, apacheLang));
+        Assertions.assertTrue(ProjectUtils.projectHasDependency(project, apacheMath));
+
+        Assertions.assertTrue(ProjectUtils.projectHasDependency(project, g1, n1, v1));
+        Assertions.assertTrue(ProjectUtils.projectHasDependency(project, g2, n2, v2));
+    }
+
+    @Test
+    public void shouldConstructValidDependencyNotations() {
+
+        java.util.Set<String> dependencyNotations = new java.util.HashSet<>();
+        for (Dependency dependency : ProjectUtils.getProjectDependencies(project))
+        {
+            String notation = ProjectUtils.getDependencyNotation(dependency);
+            /*
+             * Confirm that both method return an identical result
+             */
+            Assertions.assertEquals(notation, ProjectUtils.getDependencyNotation(
+                    dependency.getGroup(), dependency.getName(), dependency.getVersion()));
+
+            dependencyNotations.add(notation);
+        }
+        Assertions.assertTrue(dependencyNotations.contains(apacheLang));
+        Assertions.assertTrue(dependencyNotations.contains(apacheMath));
+    }
+}
